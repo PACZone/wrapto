19666f468d36e4b685eaeb6b7f7ce7896774ca4c
zigBalthazar <zigbalthazar@gmail.com>
2024-04-05 03:51:15 +0330

    feat(polygon): add bridge

---
+++ sides/polygon/bridge.go
@@ -0,0 +1,54 @@
+package polygon
+
+import (
+	"math/big"
+
+	"github.com/PACZone/wrapto/types/bypass"
+	"github.com/PACZone/wrapto/types/message"
+	"github.com/ethereum/go-ethereum/common"
+)
+
+type Bridge struct {
+	client     *Client
+	bypassName bypass.Name
+	bypass     chan message.Message
+}
+
+func NewBridge(b chan message.Message, bn bypass.Name, c *Client) Bridge {
+	return Bridge{
+		bypass:     b,
+		bypassName: bn,
+		client:     c,
+	}
+}
+
+func (b Bridge) Start() {
+	for msg := range b.bypass {
+		err := b.ProcessMsg(msg)
+		if err != nil {
+			// TODO: Log
+			continue
+		}
+	}
+}
+
+func (b *Bridge) ProcessMsg(msg message.Message) error {
+	err := msg.BasicCheck(b.bypassName)
+	if err != nil {
+		return err
+	}
+
+	payload := msg.Payload
+
+	amountBigInt := new(big.Int).SetUint64(uint64(payload.Amount()))
+
+	// TODO: log hash
+	_ , err = b.client.Mint(*amountBigInt, common.HexToAddress(payload.Receiver))
+	if err != nil{
+		//log
+		return err
+	}
+
+	return nil
+
+}
