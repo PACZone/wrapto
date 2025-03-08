f462170cb32fe36f7425ddf80a941b34537a4056
k. <me@kehiy.net>
2025-03-08 22:53:09 +0330

    fix: dest info (#84)

--- sides/pactus/utils.go
+++ sides/pactus/utils.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	"github.com/PACZone/wrapto/types/bypass"
+	"github.com/PACZone/wrapto/types/order"
 )
 
 type Dest struct {
@@ -13,25 +14,36 @@ type Dest struct {
 	Addr       string
 }
 
-func ParseMemo(memo string) (*Dest, error) {
+func ParseMemo(memo string) (Dest, error) {
 	splitMemo := strings.Split(memo, "@")
 
 	if len(splitMemo) != 2 {
-		return nil, InvalidMemoError{}
+		return Dest{}, InvalidMemoError{}
 	}
 
 	match, _ := regexp.MatchString("^(0x)?[0-9a-fA-F]{40}$", splitMemo[0])
 
 	if !match {
-		return nil, InvalidMemoError{}
+		return Dest{}, InvalidMemoError{}
 	}
 
 	if !slices.Contains(bypass.ValidDestinations, bypass.Name(splitMemo[1])) {
-		return nil, InvalidMemoError{}
+		return Dest{}, InvalidMemoError{}
 	}
 
-	return &Dest{
+	return Dest{
 		BypassName: bypass.Name(splitMemo[1]),
 		Addr:       splitMemo[0],
 	}, nil
+}
+
+func (d *Dest) GetBridgeType() order.BridgeType {
+	switch d.BypassName { //nolint
+	case bypass.POLYGON:
+		return order.PACTUS_POLYGON
+	case bypass.BSC:
+		return order.PACTUS_BSC
+	}
+
+	return ""
 }
